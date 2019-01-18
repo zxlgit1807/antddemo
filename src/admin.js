1e52a9e2d092edb7d241ab20947868656e667222
zxlgit1807 <zxl719@qq.com>
2019-01-18 16:29:07 +0800

    初始化

---
+++ src/admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './pages/style/common.less';
+import { Button } from 'antd';
+
+export default class Admin extends React.Component {
+
+    render() {
+        return(
+            <Button>点我</Button>
+        )
+    }
+}
